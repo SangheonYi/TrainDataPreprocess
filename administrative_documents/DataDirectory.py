4e5c00b19e70b241f2ea44fa5dc26d4fa1d1bc40
SangheonYi <tmzpdpdlwl@gmail.com>
2022-09-28 15:16:26 +0900

    rename: Rename project name

--- administrative_documents/DataDirectory.py
+++ administrative_documents/DataDirectory.py
@@ -2,9 +2,10 @@ import os
 
 class OriginDirectory():
     def __init__(self) -> None:
-        self.image_dir = f"origin/kor_rec/"
-        self.label_dir = f"origin/labels/"
-        self.label_list = self.list_all_files(self.label_dir, self.label_list, '.json')
+        self.image_dir = "origin/kor_rec/"
+        self.label_dir = "origin/labels/"
+        self.label_list = []
+        self.list_all_files(self.label_dir, self.label_list, '.json')
         self.label_list_len = len(self.label_list)
 
     def list_all_files(self, rootdir, file_list, extend):
@@ -14,14 +15,11 @@ class OriginDirectory():
             if os.path.isdir(joined_path):
                 self.list_all_files(joined_path, file_list, extend)
             elif file.endswith(extend) or file.endswith(extend_upper):
-                file_list.append({
-                    "path": joined_path,
-                    "name": file.split(".")[0]
-                })
+                file_list.append(joined_path)
 
 class DoneDirectory():
     def __init__(self) -> None:
-        self.image_dir = f"done/kor_rec/"
+        self.image_dir = "done/kor_rec/"
         self.createDirectory(self.image_dir)
     
     def createDirectory(self, directory):
--- administrative_documents/ocr_preprocess.py
+++ administrative_documents/ocr_preprocess.py
@@ -18,7 +18,7 @@ def print_progress(i, list_len):
         print(f'{progress}%')
 
 # save crop image and record it's label in label_contents_dict
-def image_preprocess(done_image_dir: str, origin_data: OriginData):
+def image_preprocess(done_dir: str, origin_data: OriginData):
     label_contents_dict = { key:[] for key in KEYS}
     # count = 0
     for annot_index, annotation in enumerate(origin_data.label_annotations):
@@ -27,26 +27,26 @@ def image_preprocess(done_image_dir: str, origin_data: OriginData):
         text, bbox = origin_data.parse_annotation(annotation)
         try:
             # crop image
-            origin_data.image.crop(bbox).save(done_image_dir + cropped_name)
+            origin_data.image.crop(bbox).save(done_dir + cropped_name)
             # append new label
-            label_contents_dict[key].append(f"{done_image_dir.split('/')[1]}/{cropped_name}\t{text}\n")
+            label_contents_dict[key].append(f"{done_dir.split('/')[1]}/{cropped_name}\t{text}\n")
         except Exception as e:
             print(e)
             print(f"fault {origin_data.name} at {text} for {cropped_name}")
-            print(f"img save {done_image_dir}")
+            print(f"img save {done_dir}")
     return label_contents_dict
     #     if count > 10:
     #         break
 
 # open json label and image. preprocess the opened image
-def recognition_data_preprocess(origin: OriginDirectory, done_image_dir: str, origin_label_path: str, label_files: dict):
+def recognition_data_preprocess(origin: OriginDirectory, done_dir: str, origin_label_path: str, done_label_files: dict):
     with open(origin_label_path, "r") as origin_label_file:
         origin_label_json = json.load(origin_label_file)
         origin_data = OriginData(origin_label_json, origin.image_dir)
         if origin_data.image:
-            label_contents_dict = image_preprocess(done_image_dir, origin_data)
+            label_contents_dict = image_preprocess(done_dir, origin_data)
             for key in KEYS:
-                label_files[key].write("".join(label_contents_dict[key]))
+                done_label_files[key].write("".join(label_contents_dict[key]))
                 # print(len(label_contents_dict[key]))
 
 if __name__ == "__main__":
@@ -54,18 +54,17 @@ if __name__ == "__main__":
     origin, done = OriginDirectory(), DoneDirectory()
     print("list time:", time.time() - start)
     start = time.time()
-    with open("train_list.txt", "w") as train_label, open("val_list.txt", "w") as val_label:
-        label_files = {
+    with open("done/train.txt", "w") as train_label, open("done/val.txt", "w") as val_label:
+        done_label_files = {
             "train":train_label,
             "val":val_label
         }
         for i, origin_label in enumerate(origin.label_list):
             # print(origin_label)
-            origin_label_path = origin_label["path"]
-            if not os.path.isfile(origin_label_path):
-                print('not exist label:', origin_label_path)
+            if not os.path.isfile(origin_label):
+                print('not exist label:', origin_label)
                 continue
-            recognition_data_preprocess(origin, done.image_dir, origin_label_path, label_files)
+            recognition_data_preprocess(origin, done.image_dir, origin_label, done_label_files)
             print_progress(i, origin.label_list_len)
             # if i == 0:
             #     break
