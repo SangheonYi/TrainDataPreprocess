cb12cbfdec7ee198221ee8dfaf263591a514fc41
SangheonYi <tmzpdpdlwl@gmail.com>
2022-11-07 11:11:16 +0900

    feat: Feat util

--- PDFPreprocess/util/util.py
+++ PDFPreprocess/util/util.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 def create_directory(path):
     if not os.path.exists(path):
@@ -8,3 +9,15 @@ def create_directories(paths):
     for path in paths:
         create_directory(path)
 
+def get_file_list(path):
+    for root, dir, file_list in os.walk(path):
+        return [os.path.join(root, file) for file in file_list]
+
+def logging_time(original_fn):
+    def wrapper_fn(*args, **kwargs):
+        start_time = time.time()
+        result = original_fn(*args, **kwargs)
+        end_time = time.time()
+        print("WorkingTime[{}]: {} sec".format(original_fn.__name__, end_time-start_time))
+        return result
+    return wrapper_fn
