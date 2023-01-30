0ba460eb8ab031b0d17330484dcc1c3bc842c771
SangheonYi <tmzpdpdlwl@gmail.com>
2023-01-30 12:58:11 +0900

    feat: Feat collect false image

--- PDFPreprocess/inspect_inf_result/inspect.py
+++ PDFPreprocess/inspect_inf_result/inspect.py
@@ -1,3 +1,9 @@
+from shutil import copyfile
+from pathlib import Path
+import sys
+sys.path.append("..")
+from util.util import create_directory
+
 with open('inf.log', 'r', encoding='utf-8') as log_file, open('rec_banila_train.txt', 'r', encoding='utf-8') as GT_file:
     lines = [line[:-1].split('.png:') for line in log_file.readlines()]
     gt_dict = dict()
@@ -9,7 +15,12 @@ with open('inf.log', 'r', encoding='utf-8') as log_file, open('rec_banila_train.
         result = line[1][2:].split(", 0.")[0][:-1]
         gt = gt_dict[key]
         if gt != result:
-            print(f"progress: {idx}/{len(lines)} path: {key}.png")
-            print(f"result len: {len(result)} gt len: {len(gt)}")
-            print(f"result:\t{result}")
-            print(f"gt:\t\t{gt}\n")
+            false_img_path = Path(key)
+            if not false_img_path.parent.exists():
+                create_directory(false_img_path.parent)
+            copyfile(f"../{key}.png", f"{key}.png")
+            # print(f"progress: {idx}/{len(lines)} path: {key}.png")
+            # print(f"result len: {len(result)} gt len: {len(gt)}")
+            # print(f"result:\t{result}")
+            # print(f"gt:\t\t{gt}\n")
+            # break
