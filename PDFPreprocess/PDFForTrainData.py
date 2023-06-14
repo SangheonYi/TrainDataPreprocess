25dee413680f6c44d52371349821e83cdaa79014
SangheonYi <sayidev42@fasoo.com>
2023-06-14 18:00:00 +0900

    fix: Fix coordinate name

--- PDFPreprocess/PDFForTrainData.py
+++ PDFPreprocess/PDFForTrainData.py
@@ -17,18 +17,17 @@ class PDFForTrainData():
     def cal_coor(self, bbox, img_rate):
         # image size에 맞게 bbox 비율 고려해야 함
         bbox = [coor * img_rate for coor in bbox]
-        left, lower, right, upper = bbox
+        left, pdf_upper, right, pdf_lower = bbox
         # pdf의 영점은 좌측 하단 != 이미지의 영점은 좌측 상단
         # pdf는 사용자 관점 아래에서 위로 증가하는 y축
         # 이미지는 사용자 관점 위에서 아래로 증가하는 y축
-        # 영점조절 (lower-left, upper-right)
+        # 영점조절 (lower-left, upper-right) x축으로 대칭이동해야한다.
         page_height = self.current_page.mediabox[-1]
-        upper = page_height * img_rate - upper
-        lower = page_height * img_rate - lower
+        upper = page_height * img_rate - pdf_lower
+        lower = page_height * img_rate - pdf_upper
         margin = (lower - upper) * 0.025
         # return [left - margin, upper - margin , right + margin, lower + margin * 2] # korean doc fit
         return [left - margin, upper + margin * 6, right + margin, lower - margin] # eng paper fit
-        # left, lower, right, top
 
     def get_pdf_aggregator_result(self, page):
         self.current_page = page
