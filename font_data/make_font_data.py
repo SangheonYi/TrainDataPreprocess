77f89c4bd39a2a1f7b3e699bca6eb185f9eaf97e
SangheonYi <sayidev42@fasoo.com>
2023-07-05 15:11:58 +0900

    refactor: Refactor init font once at one size

--- font_data/make_font_data.py
+++ font_data/make_font_data.py
@@ -9,18 +9,19 @@ import time
 import numpy as np
 from scipy.stats import loguniform
 
-def make_font_data(text_to_draw, font_path, encoding, save_path, font_size=10):
-    # font setting
+def font_init(font_path, encoding, font_size=10):
     encode_type = 'unic' if encoding.startswith("utf") else 'wans'
-    font = ImageFont.truetype(font_path, size=font_size, encoding=encode_type)
+    return ImageFont.truetype(font_path, size=font_size, encoding=encode_type)
+
+def make_font_data(text_to_draw, save_path, font):
     # Image size
     left, top, right, bottom = font.getbbox(text_to_draw) # warning, char code 32 space's top and bottom are same
     W = int(font.getlength(text_to_draw))
     text_coord = (0, 0)
-    if font_size < 10:
+    if font.size < 10:
         text_coord = (0, 1)
         W += 1
-    H = int((bottom - top) * 1.1) if text_to_draw != ' ' else font_size
+    H = int((bottom - top) * 1.1) if text_to_draw != ' ' else font.size
     # draw text_to_draw on image
     image =Image.new('RGB', (W, H), color = 'white')
     draw = ImageDraw.Draw(image)
@@ -41,7 +42,7 @@ def sample_size(start_point, step_size, boundary):
     drawn = (rv.rvs(1) - a) * step_size + start_point
     return int(drawn[0])
 
-def get_random_arg(target_char, support_chars, font_name, ramdom_glyph_concat):
+def get_random_gt(target_char, support_chars, font_name, ramdom_glyph_concat):
     random_chars = ''
     if ramdom_glyph_concat:
         random_chars = random.choices(support_chars, k=random.randint(15,23))
@@ -51,10 +52,25 @@ def get_random_arg(target_char, support_chars, font_name, ramdom_glyph_concat):
     is_fault = won_dict[font_name].get(target_char, False)
     if is_fault and is_fault in random_gt:
         print(f"found fault at {font_name} in ❓{random_gt}❓")
-    random_size = random.randint(8, 56)
-    return f"{target_char}{joined}", random_gt, random_size
+    return f"{target_char}{joined}", random_gt
 
-def make_fonts_dataset(font_name, font_sizes, storage_dir, ramdom_font_size=False, ramdom_glyph_concat=False):
+def make_draw_list(support_chars, save_dir, font, font_name, ramdom_glyph_concat):
+    draw_list = []
+    sub_label_lines = []
+    for idx, target_char in enumerate(support_chars):
+        char_code = ord(target_char)
+        random_chars, random_gt = get_random_gt(target_char, support_chars, font_name, ramdom_glyph_concat)
+        if font.size < 11 and is_cjk_ideographs(char_code):
+            continue
+        save_path = f'{save_dir}/{idx}_{char_code}_{font.size}size.jpg'
+        sub_label_lines.append(f"{save_path}\t{random_gt}\n")
+        draw_list.append({
+            "text_to_draw": random_chars, 
+            "save_path": save_path, 
+        })
+    return draw_list, sub_label_lines
+
+def make_fonts_dataset(font_name, font_sizes, storage_dir, ramdom_glyph_concat=False):
     label_lines = []
     korean_dict = set()
     
@@ -64,25 +80,15 @@ def make_fonts_dataset(font_name, font_sizes, storage_dir, ramdom_font_size=Fals
     start = time.time()
     if support_chars:
         for font_size in font_sizes:
-            if ramdom_font_size:
-                save_dir = f'{storage_dir}/{font_name}_random_size_data'
-            else:
-                save_dir = f'{storage_dir}/{font_name}_{font_size}_data'
+            font = font_init(font_path, encoding, font_size=font_size)
+            save_dir = f'{storage_dir}/{font_name}_{font_size}_data'
             os.makedirs(save_dir, exist_ok=True)
-            for idx, target_char in enumerate(support_chars):
-                char_code = ord(target_char)
-                if font_size < 11 and is_cjk_ideographs(char_code):
-                    continue
-                random_chars, random_gt, random_size = get_random_arg(target_char, support_chars, font_name, ramdom_glyph_concat)
-                if ramdom_font_size:
-                    font_size = random_size
-                save_path = f'{save_dir}/{idx}_{char_code}_{font_size}size.jpg'
-                make_font_data(random_chars, font_path, encoding, save_path, font_size=font_size)
-                label_lines.append(f"{save_path}\t{random_gt}\n")
-            if ramdom_font_size:
-                print(f"font: {font_name}, random font size done, spent: {time.time() - start}")
-            else:
-                print(f"font: {font_name}, font size: {font_size} done, spent: {time.time() - start}")
+            draw_list, sub_label_lines = make_draw_list(support_chars, save_dir, font, font_name, ramdom_glyph_concat)
+            label_lines += sub_label_lines
+
+            # generate font data
+            map(lambda draw_meta: make_font_data(draw_meta["text_to_draw"], draw_meta["save_path"], font), draw_list)
+            print(f"font: {font_name}, font size: {font_size} done, spent: {time.time() - start}")
     else:
         print(f"{font_path} is empty")
     return label_lines, korean_dict
@@ -98,15 +104,15 @@ if __name__ == '__main__':
     font_label_list = []
     font_dict_set = set()
 
-    font_name_list = ['hy헤드라인m']
     font_name_list = ['휴먼명조', 'Dotum', 'hy헤드라인m', 'Gungsuh', 'Batang', 'Gulim', 'HY견고딕']
+    font_name_list = ['hy헤드라인m']
     font_sizes = [10, 15, 20] # eval to small font
     font_sizes = [27, 47, 66] # 8, 14, 20 pt in 200dpi
     # 8, 10, 24 fix sizes, 11, 22, 33, 44, 55 interval random sizes
 
     step_size = 11
     font_sizes = [8, 10, 24] + [sample_size(start_point, step_size, (4, 5)) for start_point in range(11, 56, step_size)] 
-    ramdom_font_size = False
+    font_sizes = [8]
     ramdom_glyph_concat = True
 
     pool_count = os.cpu_count() // 4
@@ -117,7 +123,7 @@ if __name__ == '__main__':
         print(f"pool_count: {pool_count}")
         pool = Pool(pool_count)
         results = {font_name:pool.apply_async(make_fonts_dataset, args=
-                                              (font_name, font_sizes, storage_dir, ramdom_font_size, ramdom_glyph_concat)) 
+                                              (font_name, font_sizes, storage_dir, ramdom_glyph_concat)) 
                                               for font_name in font_name_sub_list }
         pool.close()
         pool.join()
